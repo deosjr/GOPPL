b5b3fb62f2c37986787f8b297f99086e5563998f
Sjoerd Dost <deosjr@gmail.com>
2015-02-20 12:43:31 +0100

    Nvm, quick refactor

--- goppl_test.go
+++ goppl_test.go
@@ -6,6 +6,25 @@ import (
 	"testing"
 )
 
+func evaluateQuery(t *testing.T, testAnswers []map[string]string, answer chan prolog.Alias) {
+	for _,bindings := range testAnswers {
+		alias := <- answer
+		for k,v := range alias {
+			if v.Term_to_string() != bindings[k.String()] {
+				t.Errorf("%s bound to %s, not %s", k.String(), v.Term_to_string(), bindings[k.String()])
+			}
+			delete(bindings, k.String())
+		}
+		if len(bindings) > 0 {
+			t.Errorf("Unbound input variables: %v", bindings)
+		}
+	}
+	_, open := <-answer
+	if open {
+		t.Errorf("Channel still open!")
+	}
+}
+
 //TODO: when parsing is functioning,
 //make a table driven test using filename,expected_answers
 //so we have one testfunction instead of 3 identical ones
@@ -21,22 +40,7 @@ func TestPerms(t *testing.T) {
 		{"X":"b","Y":"a"},
 		{"X":"b","Y":"b"},
 	}
-	for _,bindings := range testAnswers {
-		alias := <- answer
-		for k,v := range alias {
-			if v.Term_to_string() != bindings[k.String()] {
-				t.Errorf(k.String(), v.Term_to_string())
-			}
-			delete(bindings, k.String())
-		}
-		if len(bindings) > 0 {
-			t.Errorf("Unbound input variables: %v", bindings)
-		}
-	}
-	_, open := <-answer
-	if open {
-		t.Errorf("Channel still open!")
-	}
+	evaluateQuery(t, testAnswers, answer)
 }
 
 func TestExample(t *testing.T) {
@@ -50,22 +54,7 @@ func TestExample(t *testing.T) {
 		{"X":"b"},
 		{"X":"d"},
 	}
-	for _,bindings := range testAnswers {
-		alias := <- answer
-		for k,v := range alias {
-			if v.Term_to_string() != bindings[k.String()] {
-				t.Errorf(k.String(), v.Term_to_string())
-			}
-			delete(bindings, k.String())
-		}
-		if len(bindings) > 0 {
-			t.Errorf("Unbound input variables: %v", bindings)
-		}
-	}
-	_, open := <-answer
-	if open {
-		t.Errorf("Channel still open!")
-	}
+	evaluateQuery(t, testAnswers, answer)
 }
 
 func TestPeano(t *testing.T) {
@@ -76,20 +65,5 @@ func TestPeano(t *testing.T) {
 	testAnswers := []map[string]string{
 		{"X":"s(s(s(s(s(0)))))"},
 	}
-	for _,bindings := range testAnswers {
-		alias := <- answer
-		for k,v := range alias {
-			if v.Term_to_string() != bindings[k.String()] {
-				t.Errorf(k.String(), v.Term_to_string())
-			}
-			delete(bindings, k.String())
-		}
-		if len(bindings) > 0 {
-			t.Errorf("Unbound input variables: %v", bindings)
-		}
-	}
-	_, open := <-answer
-	if open {
-		t.Errorf("Channel still open!")
-	}
+	evaluateQuery(t, testAnswers, answer)
 }
